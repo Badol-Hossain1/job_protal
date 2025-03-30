111c75f8b8db666ec62a6da7c0ce5b88ef73620f
Badol-Hossain1 <badolhossain586@gmail.com>
2025-03-30 20:07:00 +0600

    feat: added react router dom

---
+++ src/router/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+const routers = createBrowserRouter([
+    {
+        path: '/',
+        element: <div>Hello World</div>,
+    },
+])
+
+export default routers
